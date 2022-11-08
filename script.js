709d60abfc83cede0bdfc653e64c2fc1378e5594
beicod <beicodcorp@gmail.com>
2022-11-08 16:38:12 -0500

    test translate

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+
+const translate_container = document.querySelector(".translate-container");
+
+translate_container.addEventListener("click",() => {
+    translate_container.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+
+
+
+// <!-- inicia test translater -->
+// switch lenguage
+
+const flagsElement= document.getElementById("flags")
+//identificamos el elemento, imprimiendo un console .log
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const texts = await requestJson.json();
+    console.log(texts);
+}
+
+
+
+
+
+
+
+flagsElement.addEventListener('click',(e) =>{
+    changeLanguage(e.target.parentElement.dataset.language);
+})
+
+
+
+
+// s
